914e613fdb57c2526d51a5ebdc11e42d9d92842f
Erlinis Quintana <erlinis.quintana@koombea.com>
2018-03-16 20:42:40 -0500

    Adding new post link from index

--- src/components/post_index.js
+++ src/components/post_index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { fetchPosts } from '../actions';
 import _ from 'lodash';
 
@@ -23,6 +24,11 @@ class PostsIndex extends Component {
   render() {
     return (
       <div>
+        <div className="text-xs-right">
+          <Link className="btn btn-primary" to="/posts/new">
+            Add Post
+          </Link>
+        </div>
         <h3>Post Index</h3>
         <ul className="list-group">
           {this.renderPosts()}
